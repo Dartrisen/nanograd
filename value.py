64ca148f48152455921c35c784acc750f284d905
dartrisen <calculating.scientist@gmail.com>
2024-06-24 12:14:34 +0200

    a little refactoring at the top...

--- value.py
+++ value.py
@@ -5,7 +5,8 @@ class Value:
     """Value class which stores a single scalar and it's gradient.
 
     """
-    def __init__(self, data: int | float, _children: tuple = ()) -> None:
+
+    def __init__(self, data: int | float, _children: tuple = (), _op: str = "") -> None:
         self.data = data
         self._prev = set(_children)
 
@@ -13,11 +14,11 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other: Value) -> Value:
-        out = Value(self.data + other.data, (self, other))
+        out = Value(self.data + other.data, (self, other), "+")
         return out
 
     def __mul__(self, other: Value) -> Value:
-        out = Value(self.data * other.data, (self, other))
+        out = Value(self.data * other.data, (self, other), "*")
         return out
 
 
@@ -27,4 +28,4 @@ if __name__ == '__main__':
     c = Value(10.0)
 
     d = a * b + c
-    print(d._prev)
+    print(d)
