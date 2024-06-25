8e9c72b4857ad4e388dc41e5d62ba976a0414336
dartrisen <calculating.scientist@gmail.com>
2024-06-25 18:51:25 +0200

    grad has been added

--- value.py
+++ value.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from plot_graph import draw_dot
+from math import exp
 
 
 class Value:
@@ -9,6 +10,7 @@ class Value:
 
     def __init__(self, data: int | float, _children: tuple = (), _op: str = "", label: str = "") -> None:
         self.data = data
+        self.grad = 0
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -24,15 +26,31 @@ class Value:
         out = Value(self.data * other.data, (self, other), "*")
         return out
 
+    def tanh(self):
+        x = self.data
+        t = (exp(2*x) - 1) / (exp(2*x) + 1)
+        out = Value(t, (self, ), "tanh")
+        return out
+
 
 if __name__ == '__main__':
-    a = Value(2.0, label="a")
-    b = Value(-3.0, label="b")
-    c = Value(10.0, label="c")
-
-    e = a * b
-    e.label = "e"
-    d = e + c
-    d.label = "d"
-    dot = draw_dot(d)
-    dot.render('graph', format="jpg", view=True)
+    # inputs x1, x2
+    x1 = Value(2.0, label="x1")
+    x2 = Value(0.0, label="x2")
+    # weights w1, w2
+    w1 = Value(-3.0, label="w1")
+    w2 = Value(1.0, label="w2")
+    # bias of the neuron
+    b = Value(6.7, label="b")
+    # x1*w1 + x2*w2 + b
+    x1w1 = x1 * w1
+    x1w1.label = "x1*w1"
+    x2w2 = x2 * w2
+    x2w2.label = "x2*w2"
+    x1w1x2w2 = x1w1 + x2w2
+    x1w1x2w2.label = "x1*w1 + x2*w2"
+    n = x1w1x2w2 + b
+    n.label = "n"
+    o = n.tanh()
+    dot = draw_dot(o)
+    dot.render("graph", format="jpg", view=True)
