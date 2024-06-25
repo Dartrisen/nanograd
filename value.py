6ddbcf00e878d7e29527b7a5fc3f8e9372c5176f
dartrisen <calculating.scientist@gmail.com>
2024-06-25 19:17:26 +0200

    _backward func has been added

--- value.py
+++ value.py
@@ -11,6 +11,7 @@ class Value:
     def __init__(self, data: int | float, _children: tuple = (), _op: str = "", label: str = "") -> None:
         self.data = data
         self.grad = 0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -20,16 +21,30 @@ class Value:
 
     def __add__(self, other: Value) -> Value:
         out = Value(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
         return out
 
     def __mul__(self, other: Value) -> Value:
         out = Value(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+        out._backward = _backward
         return out
 
     def tanh(self):
         x = self.data
         t = (exp(2*x) - 1) / (exp(2*x) + 1)
         out = Value(t, (self, ), "tanh")
+
+        def _backward():
+            self.grad = (1.0 - t**2) * out.grad
+        out._backward = _backward
         return out
 
 
@@ -41,7 +56,7 @@ if __name__ == '__main__':
     w1 = Value(-3.0, label="w1")
     w2 = Value(1.0, label="w2")
     # bias of the neuron
-    b = Value(6.7, label="b")
+    b = Value(6.8813735870195432, label="b")
     # x1*w1 + x2*w2 + b
     x1w1 = x1 * w1
     x1w1.label = "x1*w1"
@@ -52,5 +67,7 @@ if __name__ == '__main__':
     n = x1w1x2w2 + b
     n.label = "n"
     o = n.tanh()
+    o.grad = 1.0
+    o._backward()
     dot = draw_dot(o)
     dot.render("graph", format="jpg", view=True)
