3d4948d39e035290fd70530790e96ee03a8c9f53
dartrisen <calculating.scientist@gmail.com>
2024-06-26 18:28:56 +0200

    some features have been added

--- value.py
+++ value.py
@@ -30,6 +30,7 @@ class Value:
         return out
 
     def __mul__(self, other: Value) -> Value:
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), "*")
 
         def _backward():
@@ -38,13 +39,31 @@ class Value:
         out._backward = _backward
         return out
 
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += other * self.data**(other-1) * out.grad
+        out._backward = _backward
+        return out
+
     def __rmul__(self, other):
         return self * other
+
+    def __truediv__(self, other):
+        return self * other**(-1)
+
+    def __neg__(self):
+        return self * (-1)
+
+    def __sub__(self, other):
+        return self + (-other)
 
     def tanh(self):
         x = self.data
         t = (exp(2*x) - 1) / (exp(2*x) + 1)
-        out = Value(t, (self, ), "tanh")
+        out = Value(t, (self,), "tanh")
 
         def _backward():
             self.grad += (1.0 - t**2) * out.grad
@@ -53,7 +72,7 @@ class Value:
 
     def exp(self):
         x = self.data
-        out = Value(exp(x), (self, ), "exp")
+        out = Value(exp(x), (self,), "exp")
 
         def _backward():
             self.grad += out.data * out.grad
@@ -94,7 +113,9 @@ if __name__ == '__main__':
     x1w1x2w2.label = "x1*w1 + x2*w2"
     n = x1w1x2w2 + b
     n.label = "n"
-    o = n.tanh()
+    e = (2*n).exp()
+    o = (e - 1) / (e + 1)
+    # o = n.tanh()
     o.backward()
     # render graph
     dot = draw_dot(o)
