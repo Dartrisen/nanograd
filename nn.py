e0e7b1351d5c4a4e11ac1b4b5a6c65fbe1976e4b
dartrisen <calculating.scientist@gmail.com>
2024-06-27 17:16:54 +0200

    Neuron class has been added

--- nn.py
+++ nn.py
@@ -6,3 +6,14 @@ class Neuron:
     def __init__(self, inputs: int) -> None:
         self.w = [Value(random.uniform(-1, 1)) for _ in range(inputs)]
         self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+
+
+if __name__ == '__main__':
+    x = [2.0, 3.0]
+    n = Neuron(2)
+    print(n(x))
