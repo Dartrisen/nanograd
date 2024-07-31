9cb7815d06ece92bf3ff73766ef2f30b1169bfb4
dartrisen <calculating.scientist@gmail.com>
2024-07-31 15:17:41 +0200

    neuron.py has been created

---
+++ nanograd/neuron.py
@@ -0,0 +1,18 @@
+import random
+
+from nanograd.value import Value
+from nanograd.module import Module
+
+
+class Neuron(Module):
+    def __init__(self, inputs: int) -> None:
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(inputs)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x: list[float]):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
