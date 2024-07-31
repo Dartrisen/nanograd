763460e915b4378d1e0274ca38076bb5a1dc9fa1
dartrisen <calculating.scientist@gmail.com>
2024-07-31 17:11:25 +0200

    mlp class update

--- nanograd/mlp.py
+++ nanograd/mlp.py
@@ -1,9 +1,12 @@
 from nanograd.module import Module
 from nanograd.layer import Layer
+from nanograd.value import Value
 
 
 class MLP(Module):
-    def __init__(self, inputs: int, outputs: list[float]):
+    """A multi-layer perceptron class."""
+
+    def __init__(self, inputs: int, outputs: list[float]) -> None:
         sz = [inputs] + outputs
         self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(outputs))]
 
@@ -12,5 +15,5 @@ class MLP(Module):
             x = layer(x)
         return x
 
-    def parameters(self):
-        return [p for layer in self. layers for p in layer.parameters()]
+    def parameters(self) -> list[Value]:
+        return [p for layer in self.layers for p in layer.parameters()]
