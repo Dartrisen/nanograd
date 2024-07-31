2e9f8a1f8840fbccfbf90c5f1d4bd6d971a81bf5
dartrisen <calculating.scientist@gmail.com>
2024-07-31 18:21:01 +0200

    neuron.py tests updates

--- tests/neuron.py
+++ tests/neuron.py
@@ -11,36 +11,37 @@ class TestNeuron(unittest.TestCase):
     def setUp(self):
         """Set up Neuron."""
         self.inputs = 3
+
         with patch('random.uniform', return_value=0.5):
-            self.neuron = Neuron(self.inputs)
+            self.mock_neuron = Neuron(self.inputs)
+
+            for param in self.mock_neuron.parameters():
+                param.grad = 5
         self.x = [0.5, -0.3, 0.8]
 
     def test_initialization(self):
-        self.assertEqual(len(self.neuron.w), self.inputs, "Number of weights should match the number of inputs")
-        self.assertIsInstance(self.neuron.b, Value, "Bias should be an instance of Value")
-        for weight in self.neuron.w:
+        self.assertEqual(len(self.mock_neuron.w), self.inputs, "Number of weights should match the number of inputs")
+        self.assertIsInstance(self.mock_neuron.b, Value, "Bias should be an instance of Value")
+        for weight in self.mock_neuron.w:
             self.assertEqual(weight.data, 0.5, "Weights should be initialized to 0.5")
-        self.assertEqual(self.neuron.b.data, 0.5, "Bias should be initialized to 0.5")
+        self.assertEqual(self.mock_neuron.b.data, 0.5, "Bias should be initialized to 0.5")
 
     def test_call(self):
         with patch('nanograd.neuron.Neuron.__call__', return_value=Value(0.5)):
-            result = self.neuron(self.x)
+            result = self.mock_neuron(self.x)
             self.assertIsInstance(result, Value, "The output of Neuron should be an instance of Value")
             self.assertEqual(result.data, 0.5, msg="The output value is incorrect")
 
     def test_parameters(self):
-        params = self.neuron.parameters()
+        params = self.mock_neuron.parameters()
         self.assertEqual(len(params), self.inputs + 1, "Parameters should include all weights and the bias")
         for param in params:
             self.assertIsInstance(param, Value, "Each parameter should be an instance of Value")
 
     def test_zero_grad(self):
-        for param in self.neuron.parameters():
-            param.grad = 5
+        self.mock_neuron.zero_grad()
 
-        self.neuron.zero_grad()
-
-        for param in self.neuron.parameters():
+        for param in self.mock_neuron.parameters():
             self.assertEqual(param.grad, 0, "grad should be zeroed by zero_grad method")
 
 
